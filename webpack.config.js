fb9fe72cce86cefd71643eac71a86b0e59a30c66
Jonathan Chan <cusxio@gmail.com>
2016-07-16 20:54:29 +0800

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
     context: __dirname,
@@ -10,12 +10,24 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-            { test: /\.modernizrrc$/, loader: 'modernizr' },
         ],
     },
-    resolve: {
-        alias: {
-            modernizr$: path.resolve(__dirname, '.modernizrrc'),
-        },
-    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                unused: true,
+                dead_code: true,
+                warnings: false,
+            },
+            comments: false,
+        }),
+    ],
+
 };
